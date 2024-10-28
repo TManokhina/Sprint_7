8168bb6c97009fd32e5172ece8c55deab17a7985
Tatiana.Manokhina <tmanokhi@yandex.ru>
2024-10-29 01:22:09 +0300

    Create CourierCreds

--- src/main/java/ru/praktikumservices/qascooter/CourierCreds.java
+++ src/main/java/ru/praktikumservices/qascooter/CourierCreds.java
@@ -4,12 +4,16 @@ public class CourierCreds {
     private String login;
     private String password;
 
-    public CourierCreds(String login, String password){
+    public CourierCreds(String login, String password) {
         this.login = login;
         this.password = password;
     }
-    public static CourierCreds credsFromCourier(Courier courier){
+
+
+    public static CourierCreds credsFromCourier(Courier courier) {
         return new CourierCreds(courier.getLogin(), courier.getPassword());
     }
+
+
 
 }
